d2c9437bbf322c9a5fae9ce11f5b8a7987e3b66c
alexc21 <39373214+alexc21@users.noreply.github.com>
2018-06-01 15:24:32 +0200

    Add files via upload

---
+++ projet2/projet2.js
@@ -0,0 +1,25 @@
+            function epaule(){
+          document.getElementById("corp").src="images/epaule.png";
+           
+        }
+            function coude(){
+          document.getElementById("corp").src="images/coude.png";
+           
+        }
+            function poignet(){
+          document.getElementById("corp").src="images/poignet.png";
+           
+        }
+            function hanche(){
+          document.getElementById("corp").src="images/hanche.png";
+           
+        }
+            function genou(){
+          document.getElementById("corp").src="images/genou.png";
+           
+        }
+            function cheville(){
+          document.getElementById("corp").src="images/cheville.png";
+           var balisz = document.createElement("div");
+                
+        }

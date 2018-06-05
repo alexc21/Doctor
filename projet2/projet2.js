138371720cb503f59fc977d9df016e6ae760721e
alexc21 <39373214+alexc21@users.noreply.github.com>
2018-06-05 14:13:10 +0200

    Add files via upload

--- projet2/projet2.js
+++ projet2/projet2.js
@@ -1,13 +1,13 @@
             function epaule(){
           document.getElementById("corp").src="images/epaule.png";
           var text1=document.getElementById("bla").textContent;
-           document.getElementById("bla").textContent = "L'épaule (nom féminin) est la région morphologique se situant à la jonction du tronc avec le membre supérieur.Elle comporte plusieurs articulations qui concourent à en faire le complexe articulaire le plus mobile du corps humain. Elle permet d'orienter le membre supérieur dans l'espace, autorisant en particulier son extrémité effectrice, la main, à assurer ses rôles de préhension et de communication avec l'environnement situé à sa portée.";
+           document.getElementById("bla").textContent = "L'épaule (nom féminin) est la région morphologique se situant à la jonction du tronc avec le membre supérieur.Elle comporte plusieurs articulations qui concourent à en faire le complexe articulaire le plus mobile du corps humain.";
         }
             function coude(){
           document.getElementById("corp").src="images/coude.png";
            
           var text2=document.getElementById("bla").textContent;
-           document.getElementById("bla").textContent = " Le coude (ou articulation olécranienne) est la partie du membre supérieur située entre le bras et l'avant-bras. Cette articulation comprend en avant la région du « pli du coude » (ou fosse cubitale). C'est un complexe articulaire synovial du membre supérieur humain reliant le bras à l'avant-bras. Il unit ainsi trois os entre eux : le radius, l'ulna (cubitus) et l'humérus.Elle est la réunion de trois articulations : huméro-ulnaire, huméro-radiale et radio-ulnaire proximale (supérieure). ";
+           document.getElementById("bla").textContent = " Le coude (ou articulation olécranienne) est la partie du membre supérieur située entre le bras et l'avant-bras. Cette articulation comprend en avant la région du « pli du coude » (ou fosse cubitale). ";
         }
             function poignet(){
           document.getElementById("corp").src="images/poignet.png";
@@ -27,13 +27,13 @@
           document.getElementById("corp").src="images/genou.png";
                 
           var text5=document.getElementById("bla").textContent;
-           document.getElementById("bla").textContent = "Le genou est une articulation qui permet de joindre la jambe à la cuisse. Elle met en jeu trois os, le fémur, le tibia et la patella, par le biais de trois articulations, l'articulation fémoro-patellaire et la double articulation fémoro-tibiale. Le cartilage assure la fluidité des mouvements du genou. Le tissu élastique fin, le cartilage, protège l'os et fait en sorte que les surfaces de l'articulation glissent facilement les unes contre les autres.";
+           document.getElementById("bla").textContent = "Le genou est une articulation qui permet de joindre la jambe à la cuisse. Elle met en jeu trois os, le fémur, le tibia et la patella, par le biais de trois articulations, l'articulation fémoro-patellaire et la double articulation fémoro-tibiale. Le cartilage assure la fluidité des mouvements du genou.";
            
         }
             function cheville(){
           document.getElementById("corp").src="images/cheville.png";
           var text6=document.getElementById("bla").textContent;
-           document.getElementById("bla").textContent = "La cheville ou cou-de-pied est l'articulation qui relie la jambe et le pied.Elle est parfois sujette à des entorses, le plus souvent externes par flexion plantaire et pied en équin.Une cheville adulte est composée, au point de vue osseux, de l'épiphyse inférieure du tibia (malléole interne et plafond), de l'épiphyse inférieure de la fibula (ou péroné) (malléole externe) et du talus (ou astragale). Classiquement, on parle d'articulation supinale de l'arrière pied.On peut la définir comme une articulation synoviale de type ginglyme (anciennement trochléenne) c'est-à-dire mettant en rapport un segment de poulie plein à un segment de poulie creux.Elle ne permet comme toute trochléenne que des mouvements dans un plan sagittal : des mouvements de flexion et d'extension.";
+           document.getElementById("bla").textContent = "La cheville ou cou-de-pied est l'articulation qui relie la jambe et le pied.Elle est parfois sujette à des entorses, le plus souvent externes par flexion plantaire et pied en équin.Une cheville adulte est composée, au point de vue osseux, de l'épiphyse inférieure du tibia (malléole interne et plafond), de l'épiphyse inférieure de la fibula (ou péroné) (malléole externe) et du talus (ou astragale).";
                 
            
         }
